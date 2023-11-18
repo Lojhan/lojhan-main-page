7da858b0ebdb83746b455887e7484d43f939046b
Lojhan <viniciuslojhan@gmail.com>
2023-11-18 12:06:48 -0300

    changed message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-beween p-24 gap-6">
-      <h1 className="font-lojhan text-9xl flex-1">Lojhan</h1>
-      <span className="flex-1">{"</>"}</span>
-      <span className="font-sans">
-        Software Development • Consulting • Training
+    <main className="flex min-h-screen flex-col items-center justify-around p-24 gap-6">
+      <h1 className="font-lojhan text-9xl ">Lojhan</h1>
+      <span className="">{"</>"}</span>
+      <span className="font-sans text-center text-sm">
+        Coming soon
       </span>
     </main>
   );
