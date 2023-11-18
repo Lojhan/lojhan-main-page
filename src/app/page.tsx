d3626e41980603a16c84d7913773458441fcb517
Lojhan <viniciuslojhan@gmail.com>
2023-11-18 17:31:41 -0300

    minor color change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around p-24 gap-6">
-      <h1 className="font-lojhan text-9xl ">Lojhan</h1>
-      <span className="">{"</>"}</span>
-      <span className="font-sans text-center text-sm">Coming soon</span>
+      <h1 className="font-lojhan text-9xl text-gray-300">Lojhan</h1>
+      <span className="text-gray-300">{"</>"}</span>
+      <span className="font-sans text-center text-sm text-gray-300">Coming soon</span>
     </main>
   );
 }
