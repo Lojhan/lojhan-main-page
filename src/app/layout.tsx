4899ceae621bd4192696b1286915f39445c0b42e
lojhan <viniciuslojhan@gmail.com>
2025-05-05 09:26:20 -0300

    meta adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,67 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SiteHeader } from "@/components/site-header";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Tech Lead Portfolio",
-  description:
-    "Portfolio website for a tech lead specialist offering mentorship and consulting services",
+const metaMap = {
+  "pt-BR": {
+    title: "Lojhan - Tech Lead, Mentor e Consultor",
+    description:
+      "Tech Lead, Mentor e Consultor. Ajudo empresas a alcançarem seus objetivos através de soluções tecnológicas inovadoras.",
+  },
+  "en-US": {
+    title: "Lojhan - Tech Lead Specialist, Mentor, and Consultant",
+    description:
+      "Tech Lead Specialist, Mentor, and Consultant. I help companies achieve their goals through innovative technological solutions.",
+  },
 };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { language: "en-US" | "pt-BR" };
+}): Promise<Metadata> {
+  const { language } = params;
+  const metadata = metaMap[language];
+  return {
+    ...metadata,
+    creator: "Lojhan",
+    keywords: [
+      "lojhan",
+      "tech lead",
+      "mentor",
+      "consultant",
+      "software engineer",
+      "technology",
+      "innovation",
+    ],
+    openGraph: {
+      title: metadata.title,
+      description: metadata.description,
+      url: "https://www.lojhan.com",
+      siteName: "Lojhan",
+      images: [
+        {
+          url: "https://www.lojhan.com/lojhan-photo-home.png",
+          width: 1200,
+          height: 630,
+          alt: "Lojhan - Tech Lead, Mentor e Consultor",
+        },
+      ],
+    },
+    alternates: {
+      canonical: "https://www.lojhan.com",
+      languages: {
+        "en-US": "/en-US",
+        "pt-BR": "/pt-BR",
+      },
+    },
+  };
+}
 
 export default async function RootLayout({
   children,
